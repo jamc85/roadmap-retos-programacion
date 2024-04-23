74932c978c54d8e189c269a8ebb7ad7d9a62ecd6
jamc85 <161194769+jamc85@users.noreply.github.com>
2024-04-23 07:31:31 +0200

    Create jamc.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jamc.py
@@ -0,0 +1,22 @@
+#La página oficial de Python es https://www.python.org/
+
+#Esto es un comentario de una línea
+
+""" Esto es un comentario
+de varias líneas en bloque
+para mostrar los diferentes
+modos de hacer comentarios """
+
+num = 7 #tipo int
+num_coma = 3.4 #tipo float
+texto = "Buenos días" #tipo cadena
+encendido = True #tipo booleano
+
+""" Python como tal no soporta constantes
+pero se usan las mayúsculas para indicar
+a quien edita el código que esa variable
+sería una constante """
+DIAS_SEMANA = 7 
+
+print("¡Hola, Python!")
+
